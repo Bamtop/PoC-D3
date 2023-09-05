937c5167419ff7e9bf1b147a0b5abe331c03555c
Pablo Benitez <pablobenitezoliva01@gmail.com>
2023-09-05 11:38:56 +0200

    te toca campeon

--- src/components/D3.vue
+++ src/components/D3.vue
@@ -1,64 +1,382 @@
 <script setup>
 import * as d3 from "d3";
 import { ref, onMounted } from "vue";
+import { getPath } from "../utils";
 
 const prueba = ref();
 const data = [];
 onMounted(() => {
-  const deltaXStart = 100;
-  const deltaYStart = 100;
-  const deltaXMiddle1 = 130;
-  const deltaYMiddle1 = 100;
-  const deltaXMiddle2 = 140;
-  const deltaYMiddle2 = 180;
-  const deltaXEnd = 400;
-  const deltaYEnd = 200;
-
-  const pathString =
-    `M ${deltaXStart} ${deltaYStart} ` +
-    `Q ${deltaXMiddle1} ${deltaYMiddle1} ${deltaXMiddle2} ${deltaYMiddle2} ` +
-    `T ${deltaXEnd} ${deltaYEnd}`;
-
   const canvas = d3.select(prueba.value);
+  const rowGap = 700;
+  const radio = 50;
+  const columGap = 150;
+  const firstChildCenter = 50;
+  const startPathX = 200;
+  const startPathY = 500;
+  const endPathX = rowGap;
+  const pathWidth = 40;
+  const fontSize = 15;
+  const scoreSize = 25;
+  const scoreMargin = 20;
 
-  canvas.attr("viewBox", [0, 0, 800, 800]);
+  const textCircle = [
+    "Cookies served",
+    "Mandatory",
+    "Marketing",
+    "Personalization",
+    "Analytics",
+    "Accept-All ",
+    "Technical",
+    "Uncategorized",
+  ];
+
+  const score = [52, 14, 14, 11, 9, 8, 6, 4];
+
+  canvas.attr("viewBox", [0, 0, 1000, 1000]);
 
   const svg = canvas
     .append("svg")
     .attr("class", "svg")
-    .attr("width", 800)
-    .attr("height", 800);
-  svg
-    .append("circle")
-    .attr("cx", 25)
-    .attr("cy", 45)
-    .attr("r", 25)
-    .style("fill", "purple");
-
-  const circle = svg.append("g");
-
-  circle
-    .append("circle")
-    .attr("cx", 100)
-    .attr("cy", 100)
-    .attr("r", 10)
-    .style("fill", "red");
-
-  circle.append("title").text("hola");
-
-  svg
-    .append("circle")
-    .attr("cx", 400)
-    .attr("cy", 200)
-    .attr("r", 50)
-    .style("fill", "black");
+    .attr("width", 1000)
+    .attr("height", 1000);
 
   svg
     .append("path")
     .attr("fill", "none")
-    .style("stroke", "black")
-    .style("stroke-width", 2) // Ancho de línea de 2 unidades
-    .attr("d", pathString);
+    .style("stroke", "#406AFF26")
+    .style("stroke-width", pathWidth)
+    .style("opacity", 0.5)
+    .attr(
+      "d",
+      getPath(startPathX, startPathY, endPathX, firstChildCenter, "up", false)
+    );
+
+  svg
+    .append("path")
+    .attr("fill", "none")
+    .style("stroke", "#406AFF26")
+    .style("stroke-width", pathWidth) // Ancho de línea de 2 unidades
+    .attr(
+      "d",
+      getPath(
+        startPathX,
+        startPathY,
+        endPathX,
+        firstChildCenter + columGap,
+        "up",
+        false
+      )
+    );
+
+  svg
+    .append("path")
+    .attr("fill", "none")
+    .style("stroke", "#406AFF26")
+    .style("stroke-width", pathWidth) // Ancho de línea de 2 unidades
+    .attr(
+      "d",
+      getPath(
+        startPathX,
+        startPathY,
+        endPathX,
+        firstChildCenter + columGap * 2,
+        "up",
+        true
+      )
+    );
+
+  svg
+    .append("path")
+    .attr("fill", "none")
+    .style("stroke", "#406AFF26")
+    .style("stroke-width", pathWidth) // Ancho de línea de 2 unidades
+    .attr(
+      "d",
+      getPath(
+        startPathX,
+        startPathY,
+        endPathX,
+        firstChildCenter + columGap * 3,
+        "down",
+        true
+      )
+    );
+
+  svg
+    .append("path")
+    .attr("fill", "none")
+    .style("stroke", "#406AFF26")
+    .style("stroke-width", pathWidth) // Ancho de línea de 2 unidades
+    .attr(
+      "d",
+      getPath(
+        startPathX,
+        startPathY,
+        endPathX,
+        firstChildCenter + columGap * 4,
+        "down",
+        false
+      )
+    );
+
+  svg
+    .append("path")
+    .attr("fill", "none")
+    .style("stroke", "#406AFF26")
+    .style("stroke-width", pathWidth) // Ancho de línea de 2 unidades
+    .attr(
+      "d",
+      getPath(
+        startPathX,
+        startPathY,
+        endPathX,
+        firstChildCenter + columGap * 5,
+        "down",
+        false
+      )
+    );
+
+  svg
+    .append("path")
+    .attr("fill", "none")
+    .style("stroke", "#E04A4A26")
+    .style("stroke-width", pathWidth) // Ancho de línea de 2 unidades
+    .attr(
+      "d",
+      getPath(
+        startPathX,
+        startPathY,
+        endPathX,
+        firstChildCenter + columGap * 6,
+        "down",
+        false
+      )
+    );
+  const circleGroup1 = svg.append("g");
+  const circleGroup2 = svg.append("g");
+  const circleGroup3 = svg.append("g");
+  const circleGroup4 = svg.append("g");
+  const circleGroup5 = svg.append("g");
+  const circleGroup6 = svg.append("g");
+  const circleGroup7 = svg.append("g");
+  const circleGroup8 = svg.append("g");
+
+  circleGroup1
+    .append("circle")
+    .attr("cx", 200)
+    .attr("cy", 500)
+    .attr("r", radio)
+    .style("fill", "#87A1FA")
+    .style("z-index", "1000");
+
+  circleGroup1
+    .append("text")
+    .attr("x", 200)
+    .attr("y", 500)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "white")
+    .text(textCircle[0])
+    .style("font-size", fontSize);
+
+  circleGroup1
+    .append("text")
+    .attr("x", 200)
+    .attr("y", 500 + radio + scoreMargin)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "#696969")
+    .text(score[0])
+    .style("font-size", scoreSize);
+
+  circleGroup2
+    .append("circle")
+    .attr("cx", rowGap)
+    .attr("cy", firstChildCenter)
+    .attr("r", radio)
+    .style("fill", "#406AFF");
+
+  circleGroup2
+    .append("text")
+    .attr("x", rowGap)
+    .attr("y", firstChildCenter)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "white")
+    .text(textCircle[1])
+    .style("font-size", fontSize);
+
+  circleGroup2
+    .append("text")
+    .attr("x", rowGap + radio + scoreMargin)
+    .attr("y", firstChildCenter)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "#696969")
+    .text(score[1])
+    .style("font-size", scoreSize);
+
+  circleGroup3
+    .append("circle")
+    .attr("cx", rowGap)
+    .attr("cy", firstChildCenter + columGap)
+    .attr("r", radio)
+    .style("fill", "#406AFF");
+
+  circleGroup3
+    .append("text")
+    .attr("x", rowGap)
+    .attr("y", firstChildCenter + columGap)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "white")
+    .text(textCircle[2])
+    .style("font-size", fontSize);
+
+  circleGroup3
+    .append("text")
+    .attr("x", rowGap + radio + scoreMargin)
+    .attr("y", firstChildCenter + columGap)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "#696969")
+    .text(score[2])
+    .style("font-size", scoreSize);
+
+  circleGroup4
+    .append("circle")
+    .attr("cx", rowGap)
+    .attr("cy", firstChildCenter + columGap * 2)
+    .attr("r", radio)
+    .style("fill", "#406AFF");
+
+  circleGroup4
+    .append("text")
+    .attr("x", rowGap)
+    .attr("y", firstChildCenter + columGap * 2)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "white")
+    .text(textCircle[3])
+    .style("font-size", fontSize);
+
+  circleGroup4
+    .append("text")
+    .attr("x", rowGap + radio + scoreMargin)
+    .attr("y", firstChildCenter + columGap * 2)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "#696969")
+    .text(score[3])
+    .style("font-size", scoreSize);
+
+  circleGroup5
+    .append("circle")
+    .attr("cx", rowGap)
+    .attr("cy", firstChildCenter + columGap * 3)
+    .attr("r", radio)
+    .style("fill", "#406AFF");
+
+  circleGroup5
+    .append("text")
+    .attr("x", rowGap)
+    .attr("y", firstChildCenter + columGap * 3)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "white")
+    .text(textCircle[4])
+    .style("font-size", fontSize);
+
+  circleGroup5
+    .append("text")
+    .attr("x", rowGap + radio + scoreMargin)
+    .attr("y", firstChildCenter + columGap * 3)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "#696969")
+    .text(score[4])
+    .style("font-size", scoreSize);
+
+  circleGroup6
+    .append("circle")
+    .attr("cx", rowGap)
+    .attr("cy", firstChildCenter + columGap * 4)
+    .attr("r", radio)
+    .style("fill", "#406AFF");
+
+  circleGroup6
+    .append("text")
+    .attr("x", rowGap)
+    .attr("y", firstChildCenter + columGap * 4)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "white")
+    .text(textCircle[5])
+    .style("font-size", fontSize);
+
+  circleGroup6
+    .append("text")
+    .attr("x", rowGap + radio + scoreMargin)
+    .attr("y", firstChildCenter + columGap * 4)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "#696969")
+    .text(score[5])
+    .style("font-size", scoreSize);
+
+  circleGroup7
+    .append("circle")
+    .attr("cx", rowGap)
+    .attr("cy", firstChildCenter + columGap * 5)
+    .attr("r", radio)
+    .style("fill", "#406AFF");
+
+  circleGroup7
+    .append("text")
+    .attr("x", rowGap)
+    .attr("y", firstChildCenter + columGap * 5)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "white")
+    .text(textCircle[6])
+    .style("font-size", fontSize);
+
+  circleGroup7
+    .append("text")
+    .attr("x", rowGap + radio + scoreMargin)
+    .attr("y", firstChildCenter + columGap * 5)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "#696969")
+    .text(score[6])
+    .style("font-size", scoreSize);
+
+  circleGroup8
+    .append("circle")
+    .attr("cx", rowGap)
+    .attr("cy", firstChildCenter + columGap * 6)
+    .attr("r", radio)
+    .style("fill", "#E04A4A");
+
+  circleGroup8
+    .append("text")
+    .attr("x", rowGap)
+    .attr("y", firstChildCenter + columGap * 6)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "white")
+    .text(textCircle[7])
+    .style("font-size", fontSize);
+
+  circleGroup8
+    .append("text")
+    .attr("x", rowGap + radio + scoreMargin)
+    .attr("y", firstChildCenter + columGap * 6)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .style("fill", "#696969")
+    .text(score[7])
+    .style("font-size", scoreSize);
 });
 </script>
 <template>
---
+++ src/utils/Operations.js
@@ -0,0 +1,38 @@
+export const getPath = (
+  firstPointX,
+  firstPointY,
+  endPointX,
+  endPointY,
+  childPosition,
+  isCenter
+) => {
+  let mediumPointX1 = 0;
+  let mediumPointY1 = 0;
+  let mediumPointX2 = 0;
+  let mediumPointY2 = 0;
+  let numberX = 15;
+  let numberY = 50;
+
+  if (isCenter) {
+    numberX = 35;
+    numberY = 25;
+  }
+
+  if (childPosition === "up") {
+    mediumPointX1 = (firstPointX + endPointX) / 2 - numberX;
+    mediumPointY1 = (firstPointY + endPointY) / 2 + numberY;
+    mediumPointX2 = (firstPointX + endPointX) / 2 + numberX;
+    mediumPointY2 = (firstPointY + endPointY) / 2 - numberY;
+  } else if (childPosition === "down") {
+    mediumPointX1 = (firstPointX + endPointX) / 2 - numberX;
+    mediumPointY1 = (firstPointY + endPointY) / 2 - numberY;
+    mediumPointX2 = (firstPointX + endPointX) / 2 + numberX;
+    mediumPointY2 = (firstPointY + endPointY) / 2 + numberY;
+  }
+
+  return (
+    `M ${firstPointX} ${firstPointY} ` +
+    `Q ${mediumPointX1} ${mediumPointY1} ${mediumPointX2} ${mediumPointY2} ` +
+    `T ${endPointX} ${endPointY}`
+  );
+};
